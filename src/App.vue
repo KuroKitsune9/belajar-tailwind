4f08464d26a3626738f8a86f3078861be4ac5a68
MuharafiDalilah <muhapintar@gmail.com>
2023-07-27 13:07:11 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <navbar-component></navbar-component>
+    <head-component></head-component>
+    <main-component></main-component>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script setup>
+  import 'flowbite/dist/flowbite.js'
+
+  import NavbarComponent from './NavbarComponent.vue'
+  import HeadComponent from './HeadComponent.vue'
+  import MainComponent from './MainComponent.vue'
+  import FooterComponent from './FooterComponent.vue'
+</script>
---
+++ src/HeadComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<section class="bg-center bg-no-repeat bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUusYopZPiubqCo0wBquNFup1EyHvC_HrVTQ&usqp=CAU')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Kamen Rider</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Tempat para Tokufans melihat pendapat saya tentang kamen rider</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        </div>
+    </div>
+</section>
+
+</template>
+
+<script>
+
+</script>

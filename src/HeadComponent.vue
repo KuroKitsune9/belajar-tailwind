7253e45bb56f6f27a71091f7e080639fb36b0070
MuharafiDalilah <muhapintar@gmail.com>
2023-07-27 14:13:41 +0700

    kedua

--- src/HeadComponent.vue
+++ src/HeadComponent.vue
@@ -1,8 +1,7 @@
 <template>
 <section class="bg-center bg-no-repeat bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUusYopZPiubqCo0wBquNFup1EyHvC_HrVTQ&usqp=CAU')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Kamen Rider</h1>
-        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Tempat para Tokufans melihat pendapat saya tentang kamen rider</p>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">KuroKitsune</h1>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
         </div>
     </div>
